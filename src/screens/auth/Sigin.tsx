14d35ba97dec357d91d5707408908998d501f6ca
IjjazAhmad <ijjazahmad705@gmail.com>
2024-01-31 11:52:23 +0500

    Starter Template

---
+++ src/screens/auth/Sigin.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Sigin() {
+  return (
+    <View>
+      <Text>Sigin</Text>
+    </View>
+  )
+}
---
+++ src/screens/auth/Siginup.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Siginup() {
+  return (
+    <View>
+      <Text>Siginup</Text>
+    </View>
+  )
+}
